0d549109e9ce15f8c7442c92595f0a6069b3e437
mitu <mitu2687sp@gmail.com>
2025-03-06 16:09:42 +0900

    feat: Relationクラスをパッケージから公開

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import Model from './Model'
-import {IIndexable, IRoute, IRouteWrapper, IModel, Dictionary} from './interfaces'
+import {
+  IIndexable,
+  IRoute,
+  IRouteWrapper,
+  IModel,
+  Dictionary,
+} from './interfaces'
 import {ArrayMappable} from './entities/ArrayMappable'
 import {Paginate} from './entities/Paginate'
-import { RouteWrapper } from './entities/RouteWrapper'
-import { PaginateConfig } from './interfaces/PaginateConfig'
+import {RouteWrapper} from './entities/RouteWrapper'
+import {PaginateConfig} from './interfaces/PaginateConfig'
+import {Relation} from './Relation'
 
 export {
-	Model,
-	ArrayMappable,
-	Paginate,
-	PaginateConfig,
-	IIndexable,
-	IRouteWrapper,
-	IRoute,
-	IModel,
-	Dictionary,
-	RouteWrapper,
+  Model,
+  ArrayMappable,
+  Paginate,
+  PaginateConfig,
+  IIndexable,
+  IRouteWrapper,
+  IRoute,
+  IModel,
+  Dictionary,
+  RouteWrapper,
+  Relation,
 }
